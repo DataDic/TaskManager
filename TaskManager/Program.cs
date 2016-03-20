99afe9c63cc1663d81868586a8b985ba2da3d7e8
焰尾迭 <353166224@qq.com>
2016-03-20 16:46:17 +0800

    添加Web管理站点
    添加Web管理站点

---
+++ TaskManager/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace TaskManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {   
+            ServiceBase[] ServicesToRun = new ServiceBase[] 
+            { 
+                new TaskManagerService() 
+            };
+            //运行window任务
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
